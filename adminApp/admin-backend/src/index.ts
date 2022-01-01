93ad95285999c69b8fc323d84a82f24e9aab5451
ThiagoNelsi <thiagonelsi@gmail.com>
2022-01-01 02:29:39 -0300

    Fix cors error

--- adminApp/admin-backend/src/index.ts
+++ adminApp/admin-backend/src/index.ts
@@ -4,21 +4,26 @@ import Controller from './Controller';
 const PORT = process.env.PORT || 8080;
 
 async function setupServer() {
-    const server = http.createServer((req, res) => {
-        let body = '';
-        req.on('data', (chunk) => body += chunk);
-        req.on('end', () => {
-            if (req.headers['content-type'] === 'application/json') body = JSON.parse(body);
-            const request = Object.assign(req, { body });
-            Controller.handle(request, res);
-        });
-    });
+  const server = http.createServer((req, res) => {
 
-    await import('./controllers');
-    
-    server.listen(8080, () => { 
-        console.log('Server is running on port ' + PORT);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Max-Age', 2592000);
+
+    let body = '';
+    req.on('data', (chunk) => body += chunk);
+    req.on('end', () => {
+      if (req.headers['content-type'] === 'application/json') body = JSON.parse(body);
+      const request = Object.assign(req, { body });
+      Controller.handle(request, res);
     });
+  });
+
+  await import('./controllers');
+
+  server.listen(8080, () => {
+    console.log('Server is running on port ' + PORT);
+  });
 }
 
 setupServer();
